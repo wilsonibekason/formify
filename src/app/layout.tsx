2e46f6f8a93f589583eb661e0f1dd162a03c7fd6
Wilson Ibekason <102219342+wilsonibekason@users.noreply.github.com>
2024-07-06 10:11:00 -0700

    updated slot records

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,46 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast";
 
-export const metadata: Metadata = {
-  title: "SpeedMedia ",
-  description: "Event Registration Form",
+const meta = {
+  title: "SpeedMedia | Event Registration Form",
+  description: "SpeedMedia Event Registration Form",
+  cardImage: "/speedmedia.jpg",
+  robots: "follow, index",
+  favicon: "/favicon.ico",
+  url: "https://speedmedia-form.vercel.app/",
+  webspirrelogo: "/speedmedia.jpg",
 };
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: meta.title,
+    description: meta.description,
+    referrer: "origin-when-cross-origin",
+    keywords: ["Vercel", "Supabase", "Next.js"],
+    authors: [{ name: "Vercel", url: "https://vercel.com/" }],
+    creator: "Vercel",
+    publisher: "Vercel",
+    robots: meta.robots,
+    icons: { icon: meta.webspirrelogo },
+    metadataBase: new URL(meta.url),
+    openGraph: {
+      url: meta.url,
+      title: meta.title,
+      description: meta.description,
+      images: [meta.cardImage],
+      type: "website",
+      siteName: meta.title,
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@Vercel",
+      creator: "@Vercel",
+      title: meta.title,
+      description: meta.description,
+      images: [meta.cardImage],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
